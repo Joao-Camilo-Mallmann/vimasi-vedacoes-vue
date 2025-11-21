ece7d3b77ac46e52c8af0366a99f2b9e5e1879dd
João <joao@liquidworks.com.br>
2025-11-21 09:28:29 -0300

    feat: Improve SEO with updated meta tags, structured data, and dynamic page titles based on route.

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,18 +7,31 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: () => import('../views/HomeView.vue'),
+      meta: { title: 'Início' },
     },
     {
       path: '/vedacoes',
       name: 'vedacoes',
       component: () => import('../views/ProductsView.vue'),
+      meta: { title: 'Produtos' },
     },
     {
       path: '/contato',
       name: 'contato',
       component: () => import('../views/ContactView.vue'),
+      meta: { title: 'Contato' },
     },
   ],
+})
+
+router.beforeEach((to, from, next) => {
+  const title = to.meta.title as string
+  if (title) {
+    document.title = `${title} | Vimasi Vedações`
+  } else {
+    document.title = 'Vimasi Vedações | Vedações Hidráulicas e Industriais'
+  }
+  next()
 })
 
 export default router
