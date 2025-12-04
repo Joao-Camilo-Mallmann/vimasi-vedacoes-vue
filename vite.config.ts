93c8a6e47d0a5116f38bdb9d1fd18ea35dbcb503
João <joao@liquidworks.com.br>
2025-12-04 14:33:16 -0300

    feat: add Vite base path for deployment to sub-directory.

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vimasi-vedacoes-vue/',
   plugins: [vue(), tailwindcss(), vueDevTools({ launchEditor: 'antigravity' })],
   resolve: {
     alias: {
